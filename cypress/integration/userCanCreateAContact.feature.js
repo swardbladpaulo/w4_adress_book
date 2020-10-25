42e42603deba7eaaacfc9d59d6a5fc414cb57f37
swardbladpaulo <paulo@swardblads.se>
2020-10-25 18:16:03 +0100

    added more test and it blocks for display name and phonenumber, run test with cypress, test failed

--- cypress/integration/userCanCreateAContact.feature.js
+++ cypress/integration/userCanCreateAContact.feature.js
@@ -9,6 +9,14 @@ describe('user can create a contact', () => {
     cy.get('#notes').type('Awesome coder')
     cy.get('#twitter').type('@thomasochman')
     cy.get('#submit').click()
-  })
+  });
+
+  it('displays a name of the new contact', () => {
+    cy.get('#contact-list').should('contain', 'Thomas')
+  });
+
+  it('displays the phone number of the new contact', () => {
+    cy.get('#contact-list').should('contain', '0700 101010')
+  });
   
 })
